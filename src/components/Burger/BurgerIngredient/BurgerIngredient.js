2bd7ef5f4b7582f36b0144d562c9ca3f4319652d
Aniruddha Patil <r.patil@iitg.ac.in>
2023-05-17 14:32:36 +0530

    After Build Controls

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
-
 import PropTypes from "prop-types";
 
-import classes from "./BurgerIngredients.css";
+import classes from "./BurgerIngredient.css";
 
 class BurgerIngredient extends Component {
   render() {
@@ -20,17 +19,17 @@ class BurgerIngredient extends Component {
           </div>
         );
         break;
+      case "meat":
+        ingredient = <div className={classes.Meat}></div>;
+        break;
       case "cheese":
         ingredient = <div className={classes.Cheese}></div>;
         break;
       case "bacon":
-        ingredient = <div className={classes.bacon}></div>;
+        ingredient = <div className={classes.Bacon}></div>;
         break;
       case "salad":
         ingredient = <div className={classes.Salad}></div>;
-        break;
-      case "meat":
-        ingredient = <div className={classes.Meat}></div>;
         break;
       default:
         ingredient = null;
