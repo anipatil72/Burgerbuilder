4e8c968e2e24d4d6d5b2eecbc0bc96a2967af097
Aniruddha Patil <r.patil@iitg.ac.in>
2023-06-05 20:49:34 +0530

    Added Redux to Orders!

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-
-import { Provider } from "react-redux";
-
-import { createStore, applyMiddleware, compose } from "redux";
-
-// import reducer from "./store/reducers/burgerBuilder";
-
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
 
+import "./index.css";
+import App from "./App";
+// import registerServiceWorker from "./registerServiceWorker";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
+import orderReducer from "./store/reducers/order";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer,
+});
+
 const store = createStore(
-  burgerBuilderReducer,
+  rootReducer,
   composeEnhancers(applyMiddleware(thunk))
 );
 
@@ -33,8 +33,3 @@ const app = (
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode>{app}</React.StrictMode>);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
